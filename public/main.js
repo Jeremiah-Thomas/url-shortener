ce7a77d4207e95f250cd7b28a3fbb02affaef155
Jeremiah-Thomas <jeremiah.b.thomas12@gmail.com>
2023-08-06 18:37:16 -0500

    small tweaks to fix being able to click the link

--- public/main.js
+++ public/main.js
@@ -15,7 +15,7 @@ form.addEventListener('submit', (e) => {
         body: JSON.stringify({originalUrl: longURL})
     }).then(res => res.json()).then(json => {
         newUrl.href = `http://${json}`
-        newUrl.textContent = ${json}
+        newUrl.textContent = json
         newUrl.target="_blank"
         body.appendChild(newUrl)
 
